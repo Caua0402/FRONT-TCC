7104a6c69906be5d4177d29c61e4a4cada58497f
Cauã Felipe <110065502+Caua0402@users.noreply.github.com>
2023-11-17 11:29:26 -0300

    Atualizacao

--- app/(authenticated)/cliente/dashboard/home/page.tsx
+++ app/(authenticated)/cliente/dashboard/home/page.tsx
@@ -1,50 +1,11 @@
 "use client"
 
-import { StreamChat, User } from 'stream-chat';
-import {
-  Chat,
-  Channel,
-  ChannelHeader,
-  MessageInput,
-  MessageList,
-  Thread,
-  Window,
-} from 'stream-chat-react';
 
-const userId = 'red-sound-4';
-const userName = 'red';
-
-const user: User = {
-  id: userId,
-  name: userName,
-  image: `https://getstream.io/random_png/?id=${userId}&name=${userName}`,
-};
-
-const apiKey = 'dz5f4d5kzrue';
-const userToken =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoicmVkLXNvdW5kLTQiLCJleHAiOjE2OTkyOTY3Nzh9.-HNixV1rkgDRRV2pfueu-6fw5uXbERL5tIiQzaEz7pA';
-
-const chatClient = new StreamChat(apiKey);
-chatClient.connectUser(user, userToken);
-
-const channel = chatClient.channel('messaging', 'custom_channel_id', {
-  name: 'Talk about React',
-  members: [userId],
-});
-
-export default function HomeChat() {
+export default function Home() {
 
   return (
-    <Chat client={chatClient}>
-      <Channel channel={channel}>
-        <Window>
-          <ChannelHeader />
-          <MessageList />
-          <MessageInput />
-        </Window>
-        <Thread />
-      </Channel>
-    </Chat >
+    <>
+    </>
   )
 }
 
